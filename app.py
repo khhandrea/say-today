d6a56a99ab119e5dc382daaa3e25f0eaf3129eba
khhandrea <khhandrea@kakao.com>
2023-03-23 16:14:32 +0900

    feat: Post saying

--- app.py
+++ app.py
@@ -1,23 +1,47 @@
 from flask import Flask, jsonify, request
 from flask_restx import Api, Resource
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+import datetime
+
+
 app = Flask(__name__)
 api = Api(app)
 
-data = []
+cred = credentials.Certificate("./say-today-firebase-adminsdk.json")
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://say-today-default-rtdb.asia-southeast1.firebasedatabase.app/'})
+
 
 @api.route('/sayings')
 class Sayings(Resource):
     # get random sayings
     def get(self):
-        res = {'message': 'Think different', 'data': f'{data}'}
+        dir = db.reference('sayings')
+        res = {'message': f'{dir.get()}'}
         return jsonify(res)
     
     # post saying
     def post(self):
         payload = request.get_json()
-        data.append(payload['saying'])
-        res = {'message': f"i got {payload['saying']}!"}
+
+        dir = db.reference('system')
+        next_id = dir.get()['next-id']
+        dir.update({'next-id': next_id + 1})
+
+        data = {
+            'sayings': payload['saying'],
+            'datetime': datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+            'up': 0,
+            'down': 0
+        }
+
+        dir = db.reference(f'sayings/{next_id}')
+        dir.update(data)
+        
+        res = {'message': 'message successfully uploaded'}
         return jsonify(res)
 
 if __name__ == '__main__':
